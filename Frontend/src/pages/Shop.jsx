4070e14c7c527860c7890e2df33bfc5c3a2f1096
dkhanna99 <dhruvkhanna1104@gmail.com>
2025-06-01 11:26:59 -0700

    Finished footer

--- Frontend/src/pages/Shop.jsx
+++ Frontend/src/pages/Shop.jsx
@@ -7,9 +7,11 @@ const Shop = () => {
 
     return (
         <div className="bg-white mx-auto py-50 px-4 md:px-16 lg:px-24">
-            <h2 className="text-4xl font-bold mb-12 text-center text-black">
+            <div className="text-center mb-12">
+            <h2 className="text-4xl font-extrabold text-black inline-block border-b-4 border-red-500 pb-2">
                 All Products
             </h2>
+            </div>
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 gap-6 cursor-pointer">
                 {products.products.map((product) => (
                     <ProductCard key={product.id} product={product} />
