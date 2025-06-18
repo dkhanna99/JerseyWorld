c860f8f0858e81222e66276e823ff805ce9f11b8
dkhanna99 <dhruvkhanna1104@gmail.com>
2025-06-17 19:36:40 -0700

    Backend setup with MongoDB + category.js CRUD API

---
+++ server/app.js
@@ -0,0 +1,33 @@
+require('dotenv').config(); 
+
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+// Middleware
+app.use(cors());
+//app.options("/*", cors());
+app.use(bodyParser.json());
+
+// Routes
+const categoryRoutes = require('./routes/categories');
+
+app.use(`/api/category`, categoryRoutes);
+
+// Database
+mongoose.connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => {
+    console.log('MongoDB Connected');
+    // Server
+    app.listen(process.env.PORT, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+    }); 
+})
+.catch((err) => {
+    console.error(err);
+})
