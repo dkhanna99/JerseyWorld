494d75777a2aabed1afecacb4f8734973447e113
dkhanna99 <dhruvkhanna1104@gmail.com>
2025-06-22 23:12:43 -0700

    Added admin Dashboard Header

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Header from "./components/Header";
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
-      <h1>Dashboard React Js</h1>
+      <BrowserRouter>
+          <Header />
+        <Routes>
+          <Route path="/" exact={true} element={<Dashboard/>}/>
+            <Route path="/dashboard" exact={true} element={<Dashboard/>}/>
+            
+        </Routes>
+      </BrowserRouter>
   );
 }
 
